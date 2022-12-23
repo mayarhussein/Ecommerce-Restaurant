fd9ae9e3417046f7fc7e9bda722dcb3b11bc69d7
Mayar Hussein <mayar_memoo@ymail.com>
2022-12-22 21:51:40 -0500

    First Commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar";
+import {HeroContainer} from './HeroElements'
+
+const Hero = () => {
+    return(
+        <HeroContainer>
+            <Navbar/>
+        </HeroContainer>
+    )
+
+}
