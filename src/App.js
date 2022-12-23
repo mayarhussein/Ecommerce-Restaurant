cea906294033bc86214a886cba071959385bbeed
Mayar Hussein <mayar_memoo@ymail.com>
2022-12-23 01:21:29 -0500

    c

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
 import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero"
+import Products from "./components/Products";
+import {productData} from './components/Products/data'
+import {productDataTwo} from './components/Products/data'
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <GlobalStyle/>
        <Hero/>
+       <Products heading='Choose Your Favorite' data={productData}/>
+    <Feature/>
+    <Products heading='Sweet Treats for You' data={productDataTwo}/>
+    <Footer/>
     </Router>
   );
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../Navbar";
-import {HeroContainer} from './HeroElements'
+import Sidebar from "../Sidebar";
+import {HeroContainer, HeroContent, HeroH1, HeroP, HeroBtn, HeroItems} from './HeroElements'
 
 const Hero = () => {
+
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toggle = () => {
+      setIsOpen(!isOpen)
+   }
+
     return(
         <HeroContainer>
-            <Navbar/>
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <HeroContent>
+              <HeroItems> 
+                <HeroH1>The tastiest Pizza you'll ever eat</HeroH1>
+                <HeroP>Ready in just 7 mins</HeroP>
+                <HeroBtn>Place Order</HeroBtn>
+              </HeroItems> 
+            </HeroContent>
         </HeroContainer>
     )
 
